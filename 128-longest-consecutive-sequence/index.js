b6ef112e719b3f3e7a2d4dda6fd069d78317b080
Awyssa <michael.ehson@gmail.com>
2023-09-04 20:18:56 +0100

    fix 128 with time constraints

--- 128-longest-consecutive-sequence/index.js
+++ 128-longest-consecutive-sequence/index.js
@@ -1,45 +1,23 @@
-const loopThroughArray = (num, array) => {
-	const nextNum = array.includes(num);
-	return nextNum || false;
-};
-
 const longestConsecutive = (nums) => {
-	if (!nums.length) {
-		return 0;
+	if (!nums.length) return 0;
+
+	const set = new Set(nums);
+	let max = 0;
+
+	for (const num of set) {
+		if (set.has(num - 1)) continue;
+
+		let currNum = num;
+		let currMax = 1;
+
+		while (set.has(currNum + 1)) {
+			currNum++;
+			currMax++;
+		}
+		max = Math.max(max, currMax);
 	}
 
-	const obj = {};
-
-	for (let i = 0; i < nums.length; i++) {
-		if (obj[nums[i]]) {
-			continue;
-		}
-
-		obj[nums[i]] = [nums[i]];
-
-		let count = i;
-		let nextNumber = nums[count] + 1;
-
-		while (loopThroughArray(nextNumber, nums)) {
-			obj[nums[i]].push(nextNumber);
-			count ++;
-			nextNumber ++;
-		}
-	}
-
-	const objValues = Object.values(obj);
-
-	const longestValue =  objValues.reduce((current, acc) => {
-		if (current.length > acc.length)
-			return current;
-		else
-			return acc;
-
-	}, []);
-
-	const answer = longestValue?.length || 0;
-
-	return answer;
+	return max;
 };
 
 module.exports = { longestConsecutive };
