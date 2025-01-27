2cf73b61fa1049fcbc32a65cbe5287d34d449935
Michael H <michael.ehson@gmail.com>
2025-01-27 17:30:47 +0000

    Update sandbox.py

--- python/sandbox.py
+++ python/sandbox.py
@@ -1,23 +1,18 @@
-chocolates = [
-    {"flavour": "white", "cost": 1.50},
-    {"flavour": "milk", "cost": 1.20},
-    {"flavour": "dark", "cost": 1.80},
-    {"flavour": "vegan", "cost": 2.00},
-]
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-which_chocolate = input("Which chocolate would you like? ")
-
-# Create a new variable to save the selected chocolate
-selected_chocolate = None
-
-# Loop over all the flavours and if one matches the which_chocolate save this to the selected_chocolate
-for chocolate in chocolates:
-    if chocolate["flavour"] == which_chocolate:
-        selected_chocolate = chocolate
-        break
-
-# If we have a valid selected_chocolate print the cost, else tell the user that is not a valid flavour
-if selected_chocolate:
-  print(f"Okay, {selected_chocolate["flavour"]} chocolate costs £{selected_chocolate["cost"]}!")
-else:
-  print(f"Whoops! We do not have {which_chocolate}!")
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
