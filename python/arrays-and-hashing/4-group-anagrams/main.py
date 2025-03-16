d43bbe9ef52ecafbc32f7d1e9d1e2eacfd34a036
Awyssa <michael.ehson@gmail.com>
2025-03-16 19:55:44 +0000

    Solve  Code Testcase Test Result Test Result 49. Group Anagrams

--- python/arrays-and-hashing/4-group-anagrams/main.py
+++ python/arrays-and-hashing/4-group-anagrams/main.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = {}
+        
+        for s in strs:
+            # Create a sorted string of the characters in the current string
+            sorted_s = "".join(sorted(s))
+            
+            # If the sorted string is already in the anagram groups, append the current string to the list
+            # Otherwise, create a new list with the current string
+            if sorted_s in anagram_groups:
+                anagram_groups[sorted_s].append(s)
+            else:
+                anagram_groups[sorted_s] = [s]
+        
+        # Return the values of the anagram groups as a list of lists
+        return list(anagram_groups.values())
