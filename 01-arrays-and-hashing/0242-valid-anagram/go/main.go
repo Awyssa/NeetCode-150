268579cd53ef5b19e46a83e90846e2476b6bab0c
Michael H <michael.ehson@gmail.com>
2025-11-08 13:18:05 +0000

    valid anagram for go

--- 01-arrays-and-hashing/0242-valid-anagram/go/main.go
+++ 01-arrays-and-hashing/0242-valid-anagram/go/main.go
@@ -1,6 +1,30 @@
 package main
 
-// TODO: Implement solution
+import (
+	"sort"
+	"strings"
+)
 
-func main() {
+func isAnagram(s string, t string) bool {
+	runes1 := []rune(s)
+	runes2 := []rune(t)
+
+	sliceOne := make([]string, len(runes1))
+	sliceTwo := make([]string, len(runes2))
+
+	for i, r := range runes1 {
+		sliceOne[i] = string(r)
+	}
+
+	for i, r := range runes2 {
+		sliceTwo[i] = string(r)
+	}
+
+	sort.Strings(sliceOne)
+	sort.Strings(sliceTwo)
+
+	wordOne := strings.Join(sliceOne, "")
+	wordTwo := strings.Join(sliceTwo, "")
+
+	return wordOne == wordTwo
 }
