38dd7b65e23cb53956f4b4b7199fcaf65d14e39d
Michael H <michael.ehson@gmail.com>
2025-11-09 13:30:22 +0000

    two sum js, py

--- 01-arrays-and-hashing/0001-two-sum/js/index.js
+++ 01-arrays-and-hashing/0001-two-sum/js/index.js
@@ -1,9 +1,11 @@
-const twoSum = (nums, target) => {
-	for(let i = 0; i < nums.length; i++) {
-		for(let j = 0; j < nums.length; j++) {
-			if(nums[i] + nums[j] === target && i !== j) {
-				return [i, j];
-			}
+const twoSum = function (nums, target) {
+	const map = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		const complement = target - nums[i];
+		if (map.has(complement)) {
+			return [map.get(complement), i];
 		}
+		map.set(nums[i], i);
 	}
+	return [];
 };
--- 01-arrays-and-hashing/0001-two-sum/py/main.py
+++ 01-arrays-and-hashing/0001-two-sum/py/main.py
@@ -1,9 +1,9 @@
 class Solution:
-    def twoSum(self, nums, target):
-      prevMap = {} # val : index
-      for i, n in enumerate(nums):
-        diff = target - n
-        if diff in prevMap:
-          return [prevMap[diff], i]
-        prevMap[n] = i
-      return
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+        return []
