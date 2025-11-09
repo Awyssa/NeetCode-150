c79331d1abc5e4f70ff74181f9337bed04aacaa1
Michael H <michael.ehson@gmail.com>
2025-11-09 13:30:29 +0000

    two sum go

--- 01-arrays-and-hashing/0001-two-sum/go/main.go
+++ 01-arrays-and-hashing/0001-two-sum/go/main.go
@@ -1,4 +1,22 @@
 package main
 
+func twoSum(nums []int, target int) []int {
+
+	m := make(map[int]int)
+
+	for i, num := range nums {
+
+		complement := target - num
+
+		if j, ok := m[complement]; ok {
+			return []int{j, i}
+		}
+
+		m[num] = i
+	}
+	return []int{}
+}
+
 func main() {
+	twoSum([]int{1, 2, 3, 4, 5}, 8)
 }
